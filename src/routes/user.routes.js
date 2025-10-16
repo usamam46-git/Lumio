942290eb6e9276b336d0dcdaadd8fdffa5e6deed
usamam46-git <usamam46@gmail.com>
2025-10-16 13:12:48 +0500

    Done with user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, resgisterUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, resgisterUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router()
-
 userRouter.route("/register").post(upload.fields([
     {
         name: "avatar",
@@ -22,5 +21,20 @@ userRouter.route("/login").post(loginUser)
 userRouter.route("/logout").post(verifyJWT, logoutUser)
 //Now our logic don't really need this verifyJWT in below route because we have already done that inside user.controller.js
 userRouter.route("/refresh-token").post(refreshAccessToken)
+userRouter.route("/change-password").post(verifyJWT, changeCurrentPassword)
+//Down here we are using get method because we are just getting user and not posting anything.
+userRouter.route("/current-user").get(verifyJWT, getCurrentUser)
+userRouter.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+userRouter.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+userRouter.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+//Using this route down here because we are getting data through params in getUserChannleProfile
+
+userRouter.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+userRouter.route("/history").get(verifyJWT, getWatchHistory)
+
 
 export default userRouter
