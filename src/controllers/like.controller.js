3a6dec6dc4a6250c8bd249c6e2341873d8c3475e
usamam46-git <usamam46@gmail.com>
2025-10-18 01:21:50 +0500

    Refactor import statement for Like model to use named import

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import Like from "../models/like.model.js";
+import { Like } from "../models/like.model.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
